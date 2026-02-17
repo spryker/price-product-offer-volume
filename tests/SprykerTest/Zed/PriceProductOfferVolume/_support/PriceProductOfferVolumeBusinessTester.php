b124e58569fc8e3e85fbb1d05ce28750cd8304b0
Olha Livitchuk <olha.livitchuk@spryker.com>
2026-02-17 20:42:21 +0100

    FRW-10644 CI Fixes

--- tests/SprykerTest/Zed/PriceProductOfferVolume/_support/PriceProductOfferVolumeBusinessTester.php
+++ tests/SprykerTest/Zed/PriceProductOfferVolume/_support/PriceProductOfferVolumeBusinessTester.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace SprykerTest\Business\PriceProductOfferVolume;
+namespace SprykerTest\Zed\PriceProductOfferVolume;
 
 use Codeception\Actor;
 use Generated\Shared\Transfer\MoneyValueTransfer;
